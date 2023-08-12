544112058a1084ad2c9fe7e3193fb18f45e65678
SimJoonYeol <antihero1290@gmail.com>
2023-08-12 15:22:29 +0900

    123

--- python/STCBSTest.py
+++ python/STCBSTest.py
@@ -3,21 +3,28 @@ import multiprocessing
 
 
 if __name__ == '__main__':
-    basename_list = ["OpenEnv"]
+    basename_list = ["ConfinedEnv", "NarrowEnv", "OpenEnv", "ClutteredEnv"]
     robot_num_list = [
+        [5],
+        [2],
         [5, 10, 15, 20, 25, 30],
-        # [5, 10, 15, 20, 25, 30],
+        [5, 10, 15, 20, 25, 30],
     ]
-    count = 30
+    count = 50
     for basename, robot_nums in zip(basename_list, robot_num_list):
         for robot_num in robot_nums:
             for i in range(count):
                 benchmark_file = f"../benchmark/{basename}/{basename}_{robot_num}_{i}.yaml"
 
-                test = subprocess.Popen([f"../cmake-build-debug/ST_CBS", f"../benchmark/{basename}/{basename}_{robot_num}_{i}"], stdout=subprocess.PIPE)
+                try:
+                    test = subprocess.Popen([f"../cmake-build-debug/ST_CBS", f"{basename}", f"{robot_num}", f"{i}"], stdout=subprocess.PIPE)
+                except FileNotFoundError:
+                    print(f'File not found {basename}_{robot_num}_{i}')
+                    continue
+
                 print(f'Test case : {basename}_{robot_num}_{i}')
                 try:
-                    out, err = test.communicate(timeout=5)
+                    out, err = test.communicate(timeout=300)
                 except:
                     print(f'Timeout kill {basename}_{robot_num}_{i}')
                     test.kill()
--- python/merge_csv.py
+++ python/merge_csv.py
@@ -1,17 +1,23 @@
 if __name__ == '__main__':
-    basename_list = ["OpenEnv"]
+    basename_list = ["ConfinedEnv", "NarrowEnv", "OpenEnv", "ClutteredEnv"]
     robot_num_list = [
-        [5, 10, 15, 20, 30],
-        # [5, 10, 15, 20, 30],
+        [5],
+        [2],
+        [5, 10, 15, 20, 25, 30],
+        [5, 10, 15, 20, 25, 30],
+	[15]
     ]
-    count = 30
+    count = 50
     for basename, robot_nums in zip(basename_list, robot_num_list):
         for robot_num in robot_nums:
             new_data = []
             for i in range(count):
-                data_file = f"../raw_data/{basename}_{robot_num}_{i}_data.csv"
-                with open(data_file, 'r') as f:
-                    new_data.append(f.readline())
-            with open(f"../test_result/{basename}_{robot_num}.csv", 'a') as f:
+                data_file = f"../raw_data/{basename}/{basename}_{robot_num}_{i}_data.csv"
+                try:
+                    with open(data_file, 'r') as f:
+                        new_data.append(f.readline())
+                except FileNotFoundError:
+                    print(f'File not found {basename}_{robot_num}_{i}')
+            with open(f"../test_result/{basename}/{basename}_{robot_num}.csv", 'a') as f:
                 for new_d in new_data:
                     f.writelines(new_d + '\n')
